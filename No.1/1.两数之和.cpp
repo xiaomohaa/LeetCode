3d3c6f3e3943330ef2349fdd326ad26d85f51cfa
小默haa <guangxin_li@outlook.com>
2021-01-25 22:30:16 +0800

    第一题

--- No.1/1.两数之和.cpp
+++ No.1/1.两数之和.cpp
@@ -31,13 +31,13 @@ public:
 		map<int, int>numsMap;					// 使用map存储数据，方便查询
 		for (int i = 0; i < nums.size(); i++)
 		{
-			numsMap.insert(pair<int, int>(nums[i], i));
+			numsMap.insert(pair<int, int>(nums[i], i));		// pair 关键字将两个数据合为一个数组，配合map使用
 		}
 		vector<int> result;							// 创建一个容器存储结果
 
 		for (int i = 0; i < nums.size(); i++)		// 遍历来查询
 		{
-			//auto iter = numsMap.find(target - nums[i]);							// 可以使用auto关键字字哦东匹配参数类型
+			//auto iter = numsMap.find(target - nums[i]);							// 可以使用auto关键字匹配参数类型
 			map<int, int>::iterator iter = numsMap.find(target - nums[i]);		// 创建一个迭代器接收find查找结果
 			if (iter != numsMap.end() && iter->second != i)						// 如果查找到结果并且不是自身
 			{
